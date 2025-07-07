6b3d2e379063933eb658b1e5bf5ac0789c46f22c
Quy18 <maiquy18092003@gmail.com>
2025-07-07 15:43:25 +0700

    change migrate 2

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -17,6 +17,7 @@ class Category extends Model
     protected $fillable = [
         'name',
         'description',
+        'image_url',
     ];
     /**
      * The attributes that should be hidden for serialization.
